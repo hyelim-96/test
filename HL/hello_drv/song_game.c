4ff7cb80ed98c6c55dd1c1f929a83620c0a4a5f2
SEJIN <j240216@gmail.com>
2019-12-20 00:55:08 +0900

    remove wrong folder
    remove wrong folder

--- HL/hello_drv/song_game.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <linux/input.h>
-#include <unistd.h> // for open/close
-#include <fcntl.h> // for O_RDWR
-#include <sys/ioctl.h> // for ioctl
-#include <sys/mman.h>
